deb9f5f209af92c6167ca453dc4aeef6cc5189e8
HeyKathi <katharina.almer@sr.htlweiz.at>
2026-04-20 16:58:48 +0200

    tests ausgebessert

--- test/config/test_config_00.py
+++
@@ -1,8 +0,0 @@
-import FastAPI_TicTacToe_Game_REST_API_Development_Project
-
-
-def test_config_00() -> None:
-    config = FastAPI_TicTacToe_Game_REST_API_Development_Project.config.Config.get_instance()
-    assert config
-
-    assert "sqlite:///:memory:" == config.connection_string
