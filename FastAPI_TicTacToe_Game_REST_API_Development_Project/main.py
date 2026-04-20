7bad8fbd0f9568564a584e90216c4dd4d8c6add7
HeyKathi <katharina.almer@sr.htlweiz.at>
2026-04-20 15:37:58 +0200

    api funktioniert jetzt, muss noch an backend anpassen

--- FastAPI_TicTacToe_Game_REST_API_Development_Project/main.py
+++ FastAPI_TicTacToe_Game_REST_API_Development_Project/main.py
@@ -1,3 +1,24 @@
-from .api._app import create_app
+from fastapi import FastAPI
 
-app = create_app()
+app = FastAPI()
+
+# Variable to store text
+stored_text = ""
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello FastAPI!"}
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: str | None = None):
+    return {"item_id": item_id, "query": q}
+
+@app.put("/text")
+def store_text(text: str):
+    global stored_text
+    stored_text = text
+    return "ok"
+
+@app.get("/text")
+def get_text():
+    return stored_text
